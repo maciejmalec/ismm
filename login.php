a460179174d40044db021ac080fb1a854253dbd5
Maciej Malec <malecmaciej.mail@gmail.com>
2018-12-13 21:00:53 +0000

    first commit

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Log in</title>
+</head>
+
+<body>
+
+	<form action="loged.php" method="post">
+	
+		Login: <br/> <input type="text" name="login"/> <br/>
+		Password: <br/> <input type="password" name="password"/> <br/><br/>
+		<input type="submit" value="Log in"/>
+		
+	</form>
+
+</body>
+</html>
